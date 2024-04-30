2110c3729bf807fb2704e69a7f3c6b7cdda36ae7
Blaze Shomida <131521446+blazeshomida@users.noreply.github.com>
2024-04-30 00:52:24 -1000

    add commander and clack
    - added to tryout

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,37 @@
-export const squared = (n: number) => n * n;
+#! /bin/env node
 
-export const sum = (...num: number[]) => num.reduce((a, b) => a + b, 0);
+import { Command } from "commander";
+import { intro, outro, log, note, multiselect } from "./prompts.ts";
+import color from "picocolors";
+const program = new Command();
+program
+  .name("frame-x")
+  .description("Practice project scaffolding for any project.")
+  .version("0.0.1");
+
+program
+  .command("new")
+  .description("Create a new project.")
+  .option("--typescript", "Typescript project")
+  .action(async (opts: Record<string, string>) => {
+    note("Let's create a new project");
+    intro("Ready?");
+    log.error(color.red(JSON.stringify(opts)));
+    await multiselect({
+      message: "Select your tool(s)",
+      options: [
+        { value: "typescript", label: "Typescript" },
+        { value: "eslint", label: "ESLint" },
+        { value: "prettier", label: "Prettier" },
+      ],
+    });
+    log.info(color.blue(JSON.stringify(opts)));
+    outro("Perfect lets go!!");
+  });
+
+program
+  .command("add <tool(s)...>")
+  .description("Add a tool(s)")
+  .action((opts) => console.error(opts));
+
+program.parse(process.argv);
